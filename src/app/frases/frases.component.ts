729235efd0f74723b8a8a07ae046949e11cb8d17
André Nogueira <gorduranogueirapissuto@gmail.com>
2025-02-25 14:08:31 -0300

    atividade biscoito da sorte

---
+++ src/app/frases/frases.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frases',
+  imports: [],
+  templateUrl: './frases.component.html',
+  styleUrl: './frases.component.css'
+})
+export class FrasesComponent implements OnInit {
+
+  texto = '';
+
+  frases: string[] = [
+    "A vida é uma jornada, não um destino.",
+    "O sucesso é a soma de pequenos esforços repetidos diariamente.",
+    "Acredite em você e tudo será possível.",
+    "Grandes sonhos começam com pequenos passos.",
+    "O impossível é apenas uma opinião.",
+    "A persistência leva à perfeição.",
+    "Cada dia é uma nova oportunidade para recomeçar.",
+    "Seja a mudança que você deseja ver no mundo.",
+    "Nada é impossível para um coração determinado.",
+    "Aprenda com o passado, viva o presente e sonhe com o futuro."
+  ];
+
+  ngOnInit(): void {
+    this.trocaFrase();
+  }
+  
+  trocaFrase(){
+    this.texto = this.frases[Math.floor(Math.random() * 100) % 10];
+  }
+}
---
+++ src/app/imagem/imagem.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imagem',
+  imports: [],
+  templateUrl: './imagem.component.html',
+  styleUrl: './imagem.component.css'
+})
+export class ImagemComponent implements OnInit {
+
+  biscoitoaberto = "biscoito-aberto.png"
+  biscoitofechado = "biscoito.png"
+  currentImage: string = this.biscoitoaberto
+
+  ngOnInit(){
+    this.trocaImagem();
+  }
+
+  trocaImagem(){
+    const ImagemAtual = localStorage.getItem('currentImage');
+    if(ImagemAtual === this.biscoitoaberto){
+      this.currentImage = this.biscoitofechado;
+    }
+    else{
+      this.currentImage = this.biscoitoaberto;
+    }
+
+    localStorage.setItem('currentImage', this.currentImage)
+  }
+
+
+}
